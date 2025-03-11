f76e2a740f04c71d4139dee652d8bf897288d831
aishwarya <ashp17star@gmail.com>
2025-03-11 12:08:07 +0530

    MapsFragment hosted on Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
     namespace = "com.bitcode.a30_12_24_googlemaps_demo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bitcode.a30_12_24_googlemaps_demo"
@@ -44,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/bitcode/a30_12_24_googlemaps_demo/MainActivity.kt
+++ app/src/main/java/com/bitcode/a30_12_24_googlemaps_demo/MainActivity.kt
@@ -18,16 +18,11 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.Polygon
 import com.google.android.gms.maps.model.Polyline
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback{
+class MainActivity : AppCompatActivity(){
 
     private lateinit var activityMainBinding: ActivityMainBinding
-    private lateinit var mMap : GoogleMap
-    private lateinit var marker : Marker
-    private lateinit var bitcodeMarker : Marker
-    private lateinit var puneMarker: Marker
-    private lateinit var circle: Circle
-    private lateinit var polygon: Polygon
-    private lateinit var polyline: Polyline
+
+    private lateinit var mapsFragment: MapsFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,54 +30,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback{
         setContentView(activityMainBinding.root)
     }
 
-    override fun onMapReady(googleMap:GoogleMap) {
-        mMap = googleMap
-        initMapSettings()
-        addMarkers()
-        addShapes()
-    }
 
-    private fun addShapes(){
-        var bitcodeLatLng = LatLng(18.51021,73.83350)
-        circle = mMap.addCircle(CircleOptions()
-            .center(bitcodeLatLng)
-            .radius(500.0)
-            .strokeColor(Color.CYAN)
-            .strokeWidth(10.0F)
-            .fillColor(Color.YELLOW)
-            .zIndex(45.0F))
-    }
-
-    private fun addMarkers(){
-        var bitcodeLatLng = LatLng(18.51021,73.83350)
-
-        var markerOptions = MarkerOptions()
-
-        bitcodeMarker = mMap.addMarker(
-            markerOptions.position(bitcodeLatLng)
-                .rotation(45.0F)
-                .title("Bitcode")
-        )!!
-
-        var puneLatLng = LatLng(18.52975,73.85196)
-        puneMarker = mMap.addMarker(
-            MarkerOptions()
-                .position(puneLatLng)
-                .rotation(20.0F)
-                .title("Pune Marker"))!!
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun initMapSettings(){
-        mMap.isIndoorEnabled = true
-        mMap.isTrafficEnabled = true
-        mMap.isBuildingsEnabled = true
-        mMap.isMyLocationEnabled = true
-        mMap.uiSettings.isCompassEnabled = true
-        mMap.uiSettings.isTiltGesturesEnabled = true
-        mMap.uiSettings.isZoomControlsEnabled = true
-        mMap.uiSettings.isScrollGesturesEnabled = true
-        mMap.uiSettings.isRotateGesturesEnabled = true
-        mMap.uiSettings.isMapToolbarEnabled = true
-    }
 }
